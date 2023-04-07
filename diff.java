d850426bdeffa571dd170422c72bb237b084d2f2
Prajwal <prajwaln395@gmail.com>
2023-04-07 12:43:15 +0530

    initial commit

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class diff {
+    public static void main(String args[]) {
+        int n1 = 225, n2 = 115, sum;
+        sum = n1 - n2;
+        System.out.println("The sum of numbers is: " + sum);
+    }
+}
